e38394fd916ff635bcd50d709aae0d47f86e0647
Vasim Bala <vasimbala@hotmail.com>
2023-02-08 12:18:49 -0500

    commit5

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  location="Canada Central"  
+  virtual_network={
+    address_space="10.0.0.0/16"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "resourcegrp" {
+  name     ="${terraform.workspace}-grp"
+  location = local.location  
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = "${terraform.workspace}-network"
+  location            = local.location  
+  resource_group_name = azurerm_resource_group.resourcegrp.name
+  address_space       = [local.virtual_network.address_space]
+  depends_on = [
+    azurerm_resource_group.resourcegrp
+  ]  
+} 
+
+
+resource "azurerm_subnet" "subnet" {    
+    name                 = "${terraform.workspace}subnet"
+    resource_group_name  = azurerm_resource_group.resourcegrp.name
+    virtual_network_name = azurerm_virtual_network.network.name
+    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,0)]
+    depends_on = [
+      azurerm_virtual_network.network
+    ]
+}
+resource "azurerm_subnet" "subnetA" {    
+    name                 = "SubnetA"
+    resource_group_name  = azurerm_resource_group.resourcegrp.name
+    virtual_network_name = azurerm_virtual_network.network.name
+    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,1)]
+    depends_on = [
+      azurerm_virtual_network.network
+    ]
+}
